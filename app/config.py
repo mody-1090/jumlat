b19f505bce0613b1da732e0e510c60a726f1029d
mody-1090 <nicegoout@gmail.com>
2025-07-23 06:35:43 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key')
 
     # الآن إجباري وجود DATABASE_URL (لـ PostgreSQL)
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     QR_FOLDER = os.path.join(os.getcwd(), 'app', 'static', 'qr')
